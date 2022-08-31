fd575a4172c5ca5bdda22008f1856efe4b1b8fa2
donv <donv@inet.vn>
2022-08-31 20:56:52 +0700

    update eof

--- install_centos.sh
+++ install_centos.sh
@@ -2,4 +2,4 @@ yum -y install gcc python3 python3-pip python3-devel git
 git clone https://github.com/thesunbg/inet-tools.git
 cd inet-tools
 pip3 install -r requirements.txt
-python3 -c main.py
+python3 main.py
--- main.py
+++ main.py
@@ -14,7 +14,10 @@ def menu():
     3. Whois (All tld: .com, .vn, .net, .xyz, ...)
     4. Check host (Ping, HTTP, TCP)
     """)
-    menu_selected = input("Input tools: ")
+    try:
+        menu_selected = input("Input tools: ")
+    except EOFError:
+            pass
     if menu_selected == '0':
         print("Goodbye!")
         return False
