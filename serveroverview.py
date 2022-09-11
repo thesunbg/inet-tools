9ab7f6ed67bda1bccc848bb3f58328a9890dbca7
donv <donv@inet.vn>
2022-09-11 20:58:08 +0700

    update disk test

--- serveroverview.py
+++ serveroverview.py
@@ -1,8 +1,10 @@
 import psutil
 import platform
-import time
+from time import time
 import colorama
 import os
+import sys
+from random import shuffle
 
 def test():
     print("SYSTEM INFORMATION")
@@ -15,7 +17,7 @@ def test():
         print(colorama.Fore.LIGHTCYAN_EX +'Platform: '+colorama.Fore.LIGHTYELLOW_EX+'' +  platform.platform())
     print(colorama.Fore.LIGHTCYAN_EX +'System: ' +colorama.Fore.LIGHTYELLOW_EX+''+platform.system())
     print(colorama.Fore.LIGHTCYAN_EX +'Release: ' +colorama.Fore.LIGHTYELLOW_EX+''+platform.release())
-    print(colorama.Fore.LIGHTCYAN_EX +'Uptime: '+colorama.Fore.LIGHTYELLOW_EX+f'{(time.time() - psutil.boot_time())/DAY_SECONDS:.0f} days')
+    print(colorama.Fore.LIGHTCYAN_EX +'Uptime: '+colorama.Fore.LIGHTYELLOW_EX+f'{(time() - psutil.boot_time())/DAY_SECONDS:.0f} days')
 
     print(colorama.Fore.LIGHTCYAN_EX +'CPU Model: '+colorama.Fore.LIGHTYELLOW_EX+f'{platform.processor()}')
     print(colorama.Fore.LIGHTCYAN_EX +'CPU Number of Cores: '+colorama.Fore.LIGHTYELLOW_EX+f'{psutil.cpu_count()}')
@@ -25,38 +27,74 @@ def test():
 
     diskUsage = psutil.disk_usage('/')
     print(colorama.Fore.LIGHTCYAN_EX + 'Disk Usage: '+colorama.Fore.LIGHTYELLOW_EX+f'{diskUsage.used / (GB):.0f}/{diskUsage.total / (GB):.0f} GB ({diskUsage.percent}%)')
-
-    if platform.system() != 'Darwin':
-        #OSX is not supported.
-        procs = [p for p in psutil.process_iter()]
-        for p in procs[:]:
-            try:
-                p._before = p.io_counters()
-            except psutil.Error:
-                procs.remove(p)
-                continue
-        disks_before = psutil.disk_io_counters()
-        time.sleep(5)
-        for p in procs[:]:
-            with p.oneshot():
-                try:
-                    p._after = p.io_counters()
-                    p._cmdline = ' '.join(p.cmdline())
-                    if not p._cmdline:
-                        p._cmdline = p.name()
-                    p._username = p.username()
-                except (psutil.NoSuchProcess, psutil.ZombieProcess):
-                    procs.remove(p)
-        disks_after = psutil.disk_io_counters()
-        for p in procs:
-            p._read_per_sec = p._after.read_bytes - p._before.read_bytes
-            p._write_per_sec = p._after.write_bytes - p._before.write_bytes
-            p._total = p._read_per_sec + p._write_per_sec
-
-        disks_read_per_sec = disks_after.read_bytes - disks_before.read_bytes
-        disks_write_per_sec = disks_after.write_bytes - disks_before.write_bytes
-        print(colorama.Fore.LIGHTCYAN_EX + 'Disk read: ' + colorama.Fore.LIGHTYELLOW_EX+f'{disks_read_per_sec}')
-        print(colorama.Fore.LIGHTCYAN_EX + 'Disk write: ' + colorama.Fore.LIGHTYELLOW_EX+f'{disks_write_per_sec}')
+    
+    disk_tesk = DiskTest('/tmp/disktest', 128, 1024, 512)
+    disk_tesk.print()
     print(colorama.Fore.RESET)
     input("Press any key to exit! ")
     return False
+
+class DiskTest:
+    def __init__(self, file,write_mb, write_block_kb, read_block_kb):
+        self.file = file
+        self.write_mb = write_mb
+        self.write_block_kb = write_block_kb
+        self.read_block_kb = read_block_kb
+        wr_blocks = int(self.write_mb * 1024 / self.write_block_kb)
+        rd_blocks = int(self.write_mb * 1024 * 1024 / self.read_block_kb)
+        self.write_results = self.write_test( 1024 * self.write_block_kb, wr_blocks)
+        self.read_results = self.read_test(self.read_block_kb, rd_blocks)
+
+    def write_test(self, block_size, blocks_count, show_progress=True):
+        f = os.open(self.file, os.O_CREAT | os.O_WRONLY, 0o777)  # low-level I/O
+
+        took = []
+        for i in range(blocks_count):
+            if show_progress:
+                sys.stdout.write('\rWriting percent: {:.2f} %'.format(
+                    (i + 1) * 100 / blocks_count))
+                sys.stdout.flush()
+            buff = os.urandom(block_size)
+            start = time()
+            os.write(f, buff)
+            os.fsync(f)
+            t = time() - start
+            took.append(t)
+
+        os.close(f)
+        return took
+
+    def read_test(self, block_size, blocks_count, show_progress=True):
+        f = os.open(self.file, os.O_RDONLY, 0o777)
+        offsets = list(range(0, blocks_count * block_size, block_size))
+        shuffle(offsets)
+
+        took = []
+        for i, offset in enumerate(offsets, 1):
+            if show_progress and i % int(self.write_block_kb * 1024 / self.read_block_kb) == 0:
+                sys.stdout.write('\rReading percent: {:.2f} %'.format(
+                    (i + 1) * 100 / blocks_count))
+                sys.stdout.flush()
+            start = time()
+            os.lseek(f, offset, os.SEEK_SET) 
+            buff = os.read(f, block_size)
+            t = time() - start
+            if not buff: break
+            took.append(t)
+
+        os.close(f)
+        return took
+
+    def print(self):
+        result = ('\n\nWritten {} MB in {:.4f} s\nWrite speed is  {:.2f} MB/s'
+                    '\n  max: {max:.2f}, min: {min:.2f}\n'.format(
+            self.write_mb, sum(self.write_results), self.write_mb / sum(self.write_results),
+            max=self.write_block_kb / (1024 * min(self.write_results)),
+            min=self.write_block_kb / (1024 * max(self.write_results))))
+        result += ('\nRead {} x {} B blocks in {:.4f} s\nRead speed is  {:.2f} MB/s'
+                    '\n  max: {max:.2f}, min: {min:.2f}\n'.format(
+            len(self.read_results), self.read_block_kb,
+            sum(self.read_results), self.write_mb / sum(self.read_results),
+            max=self.read_block_kb / (1024 * 1024 * min(self.read_results)),
+            min=self.read_block_kb / (1024 * 1024 * max(self.read_results))))
+        print(result)        
